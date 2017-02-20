0c1bb25a73d5d5c13d9589497b3e1fadb74954a3
Amethyst Horn (1000038030) <HORNAJ2@student.op.ac.nz>
2017-02-20 14:42:50 +1300

    Uploaded initial files

---
+++ AlexHornAnimaChall/AlexHornAnimaChall/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlexHornAnimaChall
+{
+    class Program
+    {
+        static void red()
+        {
+            Console.BackgroundColor = ConsoleColor.Red;
+            Console.Write("  ");
+
+        }
+        static void blk()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("  ");
+
+        }
+        static void cy()
+        {
+            Console.BackgroundColor = ConsoleColor.Cyan;
+            Console.Write("  ");
+
+        }
+        static void dcy()
+        {
+            Console.BackgroundColor = ConsoleColor.DarkCyan;
+            Console.Write("  ");
+
+        }
+        static void blnk()
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.Write("  ");
+
+        }
+        static void n()
+        {
+            Console.WriteLine();
+        }
+
+        static void Main(string[] args)
+        {
+            Console.ForegroundColor = ConsoleColor.White;
+            n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); blnk(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); blnk(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); blk(); cy(); cy(); cy(); cy(); cy(); blk(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); red(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            cy(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); blnk(); blnk(); cy(); n();
+            cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); cy(); n();
+            blnk(); blnk(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); blnk(); blnk(); blnk(); cy(); cy(); cy(); cy(); n();
+
+            Console.ReadLine();
+        }
+    }
+}
